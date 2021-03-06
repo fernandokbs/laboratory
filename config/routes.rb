5ee9cb9a1bdabdbaaf513332598116d63458aace
Fernando <fernando93d@gmail.com>
2021-03-06 10:15:10 -0600

    removed type scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :types
   devise_for :users
   root to: "home#index"
 end
--- db/migrate/20210220152050_devise_create_users.rb
+++ db/migrate/20210220152050_devise_create_users.rb
@@ -8,7 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :username
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.integer :type
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
